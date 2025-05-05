2e4ddfa57078f42583fc5eb440ec0c907ff38b0f
Jacob Luszcz <jacob.luszcz@gmail.com>
2025-05-05 07:44:31 -0400

    Attempt to deploy Lambda code via GitHub Actions

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ may optionally be included.
 
 ## Status
 
-[![Status Badge](https://github.com/jluszcz/JakeSky-rs/actions/workflows/build-and-test.yml/badge.svg)](https://github.com/jluszcz/JakeSky-rs/actions/workflows/build-and-test.yml)
+[![Status Badge](https://github.com/jluszcz/JakeSky-rs/actions/workflows/build-and-deploy.yml/badge.svg)](https://github.com/jluszcz/JakeSky-rs/actions/workflows/build-and-deploy.yml)
 
 ## Supported Weather Providers
 
--- jakesky.tf
+++ jakesky.tf
@@ -58,12 +58,12 @@ data "aws_iam_policy_document" "jakesky_assume_role_policy_document" {
 }
 
 resource "aws_iam_policy" "jakesky_role_policy" {
-  name   = "jakesky"
+  name   = "jakesky.lambda"
   policy = data.aws_iam_policy_document.jakesky_role_policy_document.json
 }
 
 resource "aws_iam_role" "jakesky_role" {
-  name               = "jakesky"
+  name               = "jakesky.lambda"
   assume_role_policy = data.aws_iam_policy_document.jakesky_assume_role_policy_document.json
 }
 
@@ -77,7 +77,7 @@ resource "aws_lambda_function" "jakesky" {
   s3_bucket     = var.code_bucket
   s3_key        = "jakesky.zip"
   role          = aws_iam_role.jakesky_role.arn
-  architectures = ["arm64"]
+  architectures = ["x86_64"]
   runtime       = "provided.al2023"
   handler       = "ignored"
   publish       = "false"
@@ -106,4 +106,53 @@ resource "aws_lambda_permission" "allow_alexa" {
   function_name      = aws_lambda_function.jakesky.function_name
   principal          = "alexa-appkit.amazon.com"
   event_source_token = var.jakesky_skill_id
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_s3_bucket" "code" {
+  bucket = var.code_bucket
+}
+
+data "aws_iam_policy_document" "github_deploy" {
+  statement {
+    actions = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.code.arn}/jakesky.zip"]
+  }
+}
+
+resource "aws_iam_policy" "github" {
+  name   = "jakesky.github"
+  policy = data.aws_iam_policy_document.github_deploy.json
+}
+
+resource "aws_iam_role" "github" {
+  name = "jakesky.github"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:jluszcz/JakeSky-rs:*"
+          },
+        }
+      }
+    ]
+  })
 }
